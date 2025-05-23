0b75c8977c8d9b32c6ff124e10d23db9a04a4f95
ddasni <danielsousamelo5@gmail.com>
2025-05-23 10:10:42 -0300

    Customização do email

--- resources/views/emails/password-reset.blade.php
+++ resources/views/emails/password-reset.blade.php
@@ -1,4 +1,18 @@
-<div style="font-family: Arial, sans-serif">
-    <p>Clique no link abaixo para redefinir sua senha:</p>
-    <a href="{{ $url }}">{{ $url }}</a>
+<div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; padding: 20px; border: 1px solid #ddd; border-radius: 8px;">
+    <div style="text-align: center; margin-bottom: 10px;">
+        <img src="https://avatars.githubusercontent.com/u/203349686?s=400&u=ae376b7674563291dc1488eecf25072c6dfbb1a4&v=4" alt="Logo da Empresa" style="max-width: 170px;">
+    </div>
+
+    <h2 style="color: #333;">Redefinição de Senha</h2>
+    <p>Olá,</p>
+    <p>Recebemos uma solicitação para redefinir sua senha. Clique no botão abaixo para continuar:</p>
+
+    <p style="text-align: center;">
+        <a href="{{ $url }}" style="display: inline-block; padding: 12px 20px; background-color: #007bff; color: #fff; text-decoration: none; border-radius: 5px;">
+            Redefinir Senha
+        </a>
+    </p>
+
+    <p>Se você não solicitou isso, pode ignorar este e-mail.</p>
+    <p>Atenciosamente,<br>Promohawk</p>
 </div>
