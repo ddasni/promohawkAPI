738bcdbfe1f0141b382cfb5a656d0b3283258972
Promohawk <promohawk01@gmail.com>
2025-05-23 14:34:50 -0300

    começando o produto

--- app/Http/Controllers/Api/ProdutoController.php
+++ app/Http/Controllers/Api/ProdutoController.php
@@ -8,28 +8,26 @@ use Illuminate\Http\Request;
 class ProdutoController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Retorna uma lista de produtos.
+     *
+     * Este método recupera uma lista de produtos do banco de dados
+     * e a retorna como uma resposta JSON.
+     *
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
-        //
+        // Recupera os produtos do banco de dados, ordenados por id em ordem decrescente
+        $produtos = Produto::orderBy('id', 'DESC')->get();
+
+        // Retorna os produtos recuperados com uma resposta JSON
+        return response()->json([
+            'status' => true,
+            'produtos' => $produtos,
+        ],200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
@@ -39,13 +37,17 @@ class ProdutoController extends Controller
         //
     }
 
+
+
     /**
-     * Show the form for editing the specified resource.
+     * Store a newly created resource in storage.
      */
-    public function edit(string $id)
+    public function store(Request $request)
     {
         //
     }
+
+
 
     /**
      * Update the specified resource in storage.
@@ -54,6 +56,8 @@ class ProdutoController extends Controller
     {
         //
     }
+
+
 
     /**
      * Remove the specified resource from storage.
