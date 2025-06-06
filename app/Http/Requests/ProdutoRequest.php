fc87b7bc9ddb30d461b0579c844014dec95f64c8
ddasni <danielsousamelo5@gmail.com>
2025-06-06 10:07:49 -0300

    organizando

--- app/Http/Requests/ProdutoRequest.php
+++ app/Http/Requests/ProdutoRequest.php
@@ -57,31 +57,31 @@ class ProdutoRequest extends FormRequest
         // se for put (atualizar)
         if ($this->isMethod('put') || $this->isMethod('patch')) {
             return [
-                'nome' => 'sometimes|string|max:200',
-                'descricao' => 'sometimes|nullable|string|max:200',
-                'forma_pagamento' => 'nullable|string|max:50',
-                'parcelas' => 'nullable|integer|min:1',
-                'valor_parcela' => 'nullable|numeric|min:0',
-                'link' => 'sometimes|string',
-                'status_produto' => 'sometimes|in:ativo,inativo',
-                'imagem' => 'sometimes|string',
-                'imagens' => 'nullable|array',
-                'imagens.*' => 'string', // cada item do array deve ser uma string (base64 ou URL
+                'nome'              => 'sometimes|string|max:200',
+                'descricao'         => 'sometimes|nullable|string|max:200',
+                'forma_pagamento'   => 'nullable|string|max:50',
+                'parcelas'          => 'nullable|integer|min:1',
+                'valor_parcela'     => 'nullable|numeric|min:0',
+                'link'              => 'sometimes|string',
+                'status_produto'    => 'sometimes|in:ativo,inativo',
+                'imagem'            => 'sometimes|string',
+                'imagens'           => 'nullable|array',
+                'imagens.*'         => 'string', // cada item do array deve ser uma string (base64 ou URL)
             ];
         }
 
         //se não
         return [
-            'nome' => 'required|string|max:200',
-            'descricao' => 'nullable|string|max:200',
-            'preco' => 'required|numeric|min:0',
-            'forma_pagamento' => 'nullable|string|max:50',
-            'parcelas' => 'nullable|integer|min:1',
-            'valor_parcela' => 'nullable|numeric|min:0',
-            'imagem' => 'required|string',
-            'imagens' => 'nullable|array',
-            'imagens.*' => 'string', // valida cada imagem como base64 ou URL
-            'link' => 'required|string',
+            'nome'              => 'required|string|max:200',
+            'descricao'         => 'nullable|string|max:200',
+            'preco'             => 'required|numeric|min:0',
+            'forma_pagamento'   => 'nullable|string|max:50',
+            'parcelas'          => 'nullable|integer|min:1',
+            'valor_parcela'     => 'nullable|numeric|min:0',
+            'imagem'            => 'required|string',
+            'imagens'           => 'nullable|array',
+            'imagens.*'         => 'string',
+            'link'              => 'required|string',
         ];
     }
 
@@ -94,17 +94,17 @@ class ProdutoRequest extends FormRequest
     public function messages():array
     {
         return[
-            'nome.required' => 'Campo nome do produto é obrigatório!',
-            'nome.max' => 'Excedeu mais de :max caracteres no campo nome!',
-            'descricao.max' => 'Excedeu mais de :max caracteres no campo descrição!',
-            'preco.required' => 'Campo preco do produto é obrigatório!',
-            'imagem.required' => 'Campo imagem do produto é obrigatório!',
-            'link.required' => 'Campo url do produto é obrigatório!',
-            'forma_pagamento.max' => 'O campo forma de pagamento não pode ter mais de :max caracteres.',
-            'parcelas.integer' => 'O número de parcelas deve ser um número inteiro.',
+            'nome.required'         => 'Campo nome do produto é obrigatório!',
+            'nome.max'              => 'Excedeu mais de :max caracteres no campo nome!',
+            'descricao.max'         => 'Excedeu mais de :max caracteres no campo descrição!',
+            'preco.required'        => 'Campo preco do produto é obrigatório!',
+            'imagem.required'       => 'Campo imagem do produto é obrigatório!',
+            'link.required'         => 'Campo url do produto é obrigatório!',
+            'forma_pagamento.max'   => 'O campo forma de pagamento não pode ter mais de :max caracteres.',
+            'parcelas.integer'      => 'O número de parcelas deve ser um número inteiro.',
             'valor_parcela.numeric' => 'O valor da parcela deve ser um número.',
-            'imagens.array' => 'As imagens devem estar em um array.',
-            'imagens.*.string' => 'Cada imagem deve ser uma string (base64 ou URL).',
+            'imagens.array'         => 'As imagens devem estar em um array.',
+            'imagens.*.string'      => 'Cada imagem deve ser uma string (base64 ou URL).',
         ];
     }
 }
